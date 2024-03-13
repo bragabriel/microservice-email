7bce69f2ea43272197375024792c23823370e3e7
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-03-13 11:29:25 -0300

    initial commit

---
+++ src/main/java/com/gabriel/emailmicroservice/EmailMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.emailmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailMicroserviceApplication.class, args);
+	}
+
+}
